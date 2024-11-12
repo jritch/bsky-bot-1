7f2b30a5e1dbb933e196cc7c3fbf6d023f08b569
Jacob Ritchie <jacob.w.k.ritchie@gmail.com>
2024-11-11 20:01:54 -0500

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -3,4 +3,9 @@ import getPostText from "./lib/getPostText.js";
 
 const text = await Bot.run(getPostText, { dryRun: false });
 
-console.log(`[${new Date().toISOString()}] Posted: "${text}"`);
+if (text !== null) {
+  console.log(`[${new Date().toISOString()}] Posted: "${text}"`);
+}
+else {
+  console.log (`It's five o'clock nowhere`);
+}
